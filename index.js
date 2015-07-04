89999e9ffce42c2fef1fd9ed022efbc100690b07
meikidd <meikidd@gmail.com>
2015-07-04 20:08:27 +0800

    view.js to be continue

--- index.js
+++ index.js
@@ -3,10 +3,12 @@
 const views = require('koa-views')
 const asset = require('./lib/asset')
 const router = require('./lib/router')
+const view = require('./lib/view')
 
 
 module.exports = function(app) {
 
+  view(app)
   asset(app)
   router(app)
 
