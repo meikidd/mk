a0f67a0fea07896a415f53b9bfb9583fc8a117f4
meikidd <meikidd@gmail.com>
2015-07-05 21:17:25 +0800

    es6 class

--- index.js
+++ index.js
@@ -3,17 +3,43 @@
 const serverRoot = process.cwd()
 const resolve = require('path').resolve
 
+const koa = require('koa')
+const session = require('koa-session')
+
+const view = require('./lib/view')
 const asset = require('./lib/asset')
 const router = require('./lib/router')
-const view = require('./lib/view')
 
+class Remote {
 
-module.exports = function(app) {
-  var config = require(resolve(serverRoot, './config'))
+  constructor(middleware) {
 
-  view(app, config.views)
-  asset(app, config.assetsRoots)
-  router(app)
+    var app = koa()
+    var config = require(resolve(serverRoot, './config'))
+    app.keys = [config.appname]
 
-  return app
+    // session
+    if(config.sessionEnable) {
+      app.use(session({
+        key: app.keys[0]
+      }, app))
+    }
+
+    // middleware
+    app = middleware(app)
+
+    // templates
+    view(app, config.views)
+
+    // static server
+    asset(app, config.assetsRoots)
+
+    // router
+    router(app)
+
+    return app
+  }
+
 }
+
+module.exports = Remote
