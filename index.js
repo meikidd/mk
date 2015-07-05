ad19c357e6edf99912439ed325fca5a689744fc0
meikidd <meikidd@gmail.com>
2015-07-05 14:19:37 +0800

    add view

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 'use strict';
 
-const views = require('koa-views')
+const serverRoot = process.cwd()
+const resolve = require('path').resolve
+
 const asset = require('./lib/asset')
 const router = require('./lib/router')
 const view = require('./lib/view')
 
 
 module.exports = function(app) {
+  var config = require(resolve(serverRoot, './config'))
 
-  view(app)
-  asset(app)
+  view(app, config.views)
+  asset(app, config.assetsRoots)
   router(app)
 
   return app
